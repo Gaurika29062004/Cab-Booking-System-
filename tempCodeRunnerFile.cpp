b6b7c4bb48370e54b440bbbe996e0685bfcd7ffa
Gaurika29062004 <154672036+Gaurika29062004@users.noreply.github.com>
2025-08-26 02:33:16 +0530

    Delete tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-void resetAvailability() {
-    for (auto &kv : cabs) kv.second.available = true;
-    activeRides.clear();  // Important: remove any active rides
-    saveCabs();
-    cout << "All cabs set to available.\n";
-}
